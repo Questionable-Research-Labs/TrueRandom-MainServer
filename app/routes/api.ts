218e033a0580ad7367e2b2bd1673dc83a5047035
Jacobtread <jacobtread@gmail.com>
2021-11-21 00:59:28 +1300

    Finished new website

--- app/routes/api.ts
+++ app/routes/api.ts
@@ -1,6 +1,5 @@
-import {Request, Response, Router} from "express"
-import rateLimit, {RateLimit} from "express-rate-limit"
-import {v4 as uuidv4} from "uuid";
+import { Request, Response, Router } from "express"
+import { v4 } from "uuid";
 import DiceController from "../controller";
 
 export default (controller: DiceController) => {
@@ -8,11 +7,11 @@ export default (controller: DiceController) => {
     const router: Router = Router(); // Create a new router
 
     router.get('/random', (req: Request, res: Response) => {
-        controller.request(uuidv4(), (value: number) => res.send({response: value}))
+        controller.request(v4(), (value: number) => res.send({ response: value }))
     });
 
     router.get('/twitch/random', (req: Request, res: Response) => {
-        controller.request(uuidv4(), (value: number) => res.send({response: value}), false);
+        controller.request(v4(), (value: number) => res.send({ response: value }), false);
     });
 
     return router;
